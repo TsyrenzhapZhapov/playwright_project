707f21394815c15f169061c5588ac2684634d63a
yukazimirchenko <133990960+yukazimirchenko@users.noreply.github.com>
2024-11-07 18:11:41 +0300

    Update download.spec.js

--- tests/firstTests/download.spec.js
+++ tests/firstTests/download.spec.js
@@ -5,7 +5,8 @@ test.describe('Download', () => {
     test('download', async ({ page }) => {
         await page.goto('https://the-internet.herokuapp.com/download');
         await page.getByRole('link', { name: 'evening.png'}).click();
-
+        // complete the tests
+        // add assertions 
     });
 
 })
