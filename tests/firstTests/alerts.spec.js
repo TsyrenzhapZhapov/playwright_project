2161832ed962b7caa8ab020f42a05b15eb847351
yukazimirchenko <133990960+yukazimirchenko@users.noreply.github.com>
2024-11-07 18:09:38 +0300

    Update alerts.spec.js

--- tests/firstTests/alerts.spec.js
+++ tests/firstTests/alerts.spec.js
@@ -3,9 +3,12 @@ import { Alerts } from '../../src/locator/alerts';
 
 test.describe('Alerts', () => {
 
-    test('alert', async ({ page }) => {
+    test.beforeEach(async() => {
         const alerts = new Alerts(page);
         await alerts.goto();
+    })
+    
+    test('alert', async ({ page }) => {
         page.on('dialog', dialog => dialog.accept());
         await alerts.buttonAlert();
         await expect(alerts.successMessage).toContainText('You successfully clicked an alert');
@@ -13,8 +16,8 @@ test.describe('Alerts', () => {
 
 
     test('comfirm', async ({page}) => {
-        const alerts = new Alerts(page);
-        await alerts.goto();
+        // const alerts = new Alerts(page);
+        // await alerts.goto();
         page.on('dialog', dialog => dialog.accept());
         await alerts.buttonConfirm();
         await expect(alerts.successMessage).toContainText('You clicked: Ok');
@@ -22,12 +25,10 @@ test.describe('Alerts', () => {
 
 
     test('prompt', async ({page}) => {
-        const alerts = new Alerts(page);
-        await alerts.goto();
+        // const alerts = new Alerts(page);
+        // await alerts.goto();
         page.on('dialog', dialog => dialog.accept('test'));
         await alerts.buttonPrompt();
         await expect(alerts.successMessage).toContainText('You entered: test');
   })
-
-
 })
