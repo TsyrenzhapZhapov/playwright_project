378c1bbdeb4fb111a806bce199652be00790be77
yukazimirchenko <133990960+yukazimirchenko@users.noreply.github.com>
2024-11-07 18:11:01 +0300

    Update sign_in.spec.js

--- tests/firstTests/sign_in.spec.js
+++ tests/firstTests/sign_in.spec.js
@@ -4,25 +4,30 @@ import { signInData } from '../../testData/sign_in';
 
 test.describe('Sign in', () => {
 
-    test('positive login', async ({ page }) => {
+    test.beforeEach(async () => {
         const SignIn = new signIn(page);
         await SignIn.goto();
+    })
+
+    test('positive login', async ({ page }) => {
+        // const SignIn = new signIn(page);
+        // await SignIn.goto();
         await SignIn.login(signInData.username, signInData.password);
         await expect(SignIn.successMessage).toContainText('You logged into a secure area');
     });
 
 
     test('negative login', async ({page}) => {
-        const SignIn = new signIn(page);
-        await SignIn.goto();
+        // const SignIn = new signIn(page);
+        // await SignIn.goto();
         await SignIn.login(signInData.incorrectUsername, signInData.password);
         await expect(SignIn.successMessage).toContainText('Your username is invalid!');
     })
 
 
     test('logout', async ({page}) => {
-        const SignIn = new signIn(page);
-        await SignIn.goto();
+        // const SignIn = new signIn(page);
+        // await SignIn.goto();
         await SignIn.login(signInData.username, signInData.password);
         await SignIn.logout();
         await expect(SignIn.successMessage).toContainText('You logged out of the secure');
